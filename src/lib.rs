94ba8855d0b3e149bec9070acac03149d2da74a9
Ethan Buchman <ethan@coinculture.info>
2018-12-22 14:04:03 -0500

    remove height

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,13 @@
 
+// Value is the value the consensus seeks agreement on.
+#[derive(Copy, Clone, PartialEq)]
+struct Value{}
+
 // RoundValue contains a Value and the round it was set.
 #[derive(Copy, Clone, PartialEq)]
 struct RoundValue{
     round: i64,
     value: Value
-}
-// Value is the value the consensus seeks agreement on.
-#[derive(Copy, Clone, PartialEq)]
-struct Value{}
-
-
-// ValueManager gets and validates values.
-trait ValueManager{
-    fn get_value(&self) -> Value;
-    fn validate(&self, v: Value) -> bool;
 }
 
 // State is the state of the consensus.
@@ -42,6 +36,12 @@ impl State{
     }
 }
 
+// ValueManager gets and validates values.
+trait ValueManager{
+    fn get_value(&self) -> Value;
+    fn validate(&self, v: Value) -> bool;
+}
+
 // StateWrapper contains the State. It also contains a ValueManager 
 // for proposing a new value and validating received values.
 #[derive(Copy, Clone)]
@@ -64,21 +64,20 @@ enum RoundStep {
 
 // Event causes a state transition.
 enum Event {
-    NewHeight(i64),
-    NewRound(i64, i64),
-    NewRoundProposer(i64, i64),
-    Proposal(i64, i64, Value),
-    ProposalPolka(i64, i64, i64, Value),
-    PolkaAny(i64, i64),
-    PolkaNil(i64, i64),
-    PolkaValue(i64, i64, Value),
-    CommitAny(i64, i64),
-    CommitNil(i64, i64),
-    CommitValue(i64, i64, Value),
-    CertValue(i64, i64, Value),
-    TimeoutPropose(i64, i64),
-    TimeoutPrevote(i64, i64),
-    TimeoutPrecommit(i64, i64),
+    NewRound(i64),
+    NewRoundProposer(i64),
+    Proposal(i64, Value),
+    ProposalPolka(i64, i64, Value),
+    PolkaAny(i64),
+    PolkaNil(i64),
+    PolkaValue(i64, Value),
+    CommitAny(i64),
+    CommitNil(i64),
+    CommitValue(i64, Value),
+    CertValue(i64, Value),
+    TimeoutPropose(i64),
+    TimeoutPrevote(i64),
+    TimeoutPrecommit(i64),
 }
 
 // Message is returned.
@@ -91,16 +90,14 @@ enum Message {
 }
 
 struct Proposal{
-    height: i64,
     round: i64,
     value: Value,
     pol_round: i64,
 }
 
 impl Proposal{
-    fn new(height: i64, round: i64, value: Value, pol_round: i64) -> Proposal{
+    fn new(round: i64, value: Value, pol_round: i64) -> Proposal{
         Proposal{
-            height: height,
             round: round,
             value: value,
             pol_round: pol_round,
@@ -109,15 +106,13 @@ impl Proposal{
 }
 
 struct Vote{
-    height: i64,
     round: i64,
     value: Option<Value>,
 }
 
 impl Vote {
-    fn new(height: i64, round: i64, value: Option<Value>) -> Vote{
+    fn new(round: i64, value: Option<Value>) -> Vote{
         Vote{
-            height: height,
             round: round,
             value: value,
         }
@@ -125,15 +120,13 @@ impl Vote {
 }
 
 struct Timeout{
-    height: i64,
     round: i64,
     step: RoundStep,
 }
 
 impl Timeout{
-    fn new(height: i64, round: i64, step: RoundStep) -> Timeout{
+    fn new(round: i64, step: RoundStep) -> Timeout{
         Timeout{
-            height: height,
             round: round,
             step: step,
         }
@@ -167,20 +160,20 @@ impl<V> StateWrapper<V>
     fn next(self, event: Event) -> (StateWrapper<V>, Option<Message>) {
         let s = self.state;
         let (s, m) = match (s.step, event) {
-            (RoundStep::NewRound, Event::NewRoundProposer(h, r)) => {   self.handle_new_round_proposer(h, r) } // 11/14
-            (RoundStep::NewRound, Event::NewRound(h, r)) => {   self.handle_new_round(h, r) } // 11/20
-            (RoundStep::Propose, Event::Proposal(h, r, v)) => {   self.handle_proposal(h, r, v) } // 22
-            (RoundStep::Propose, Event::ProposalPolka(h, r, vr, v)) => {  self.handle_proposal_polka(h, r, vr, v) } // 28
-            (RoundStep::Propose, Event::TimeoutPropose(h, r)) => {  handle_timeout_propose(s, h, r) } // 57
-            (RoundStep::Prevote, Event::PolkaAny(h, r)) => {  handle_polka_any(s, h, r) } // 34
-            (RoundStep::Prevote, Event::PolkaNil(h, r)) => {  handle_polka_nil(s, h, r) } // 44
-            (RoundStep::Prevote, Event::PolkaValue(h, r, v)) => {  handle_polka_value(s, h, r, v) } // 36
-            (RoundStep::Prevote, Event::TimeoutPrevote(h, r)) => {  handle_timeout_prevote(s, h, r) } // 61
-            (RoundStep::Precommit, Event::PolkaValue(h, r, v)) => {  handle_polka_value(s, h, r, v) } // 36/42
-            (_,                    Event::CommitAny(h, r)) => {  handle_commit_any(s, h, r) } // 47
-            (_,                    Event::CommitValue(h, r, v)) => {  handle_commit_value(s, h, r, v) } // 49
-            (_,                    Event::CertValue(h, r, v)) => {  handle_cert_value(s, h, r, v) } // 55
-            (_,                    Event::TimeoutPrecommit(h, r)) => {  handle_timeout_precommit(s, h, r) } // 65
+            (RoundStep::NewRound, Event::NewRoundProposer(r)) => {   self.handle_new_round_proposer(r) } // 11/14
+            (RoundStep::NewRound, Event::NewRound(r)) => {   self.handle_new_round(r) } // 11/20
+            (RoundStep::Propose, Event::Proposal(r, v)) => {   self.handle_proposal(r, v) } // 22
+            (RoundStep::Propose, Event::ProposalPolka(r, vr, v)) => {  self.handle_proposal_polka(r, vr, v) } // 28
+            (RoundStep::Propose, Event::TimeoutPropose(r)) => {  handle_timeout_propose(s, r) } // 57
+            (RoundStep::Prevote, Event::PolkaAny(r)) => {  handle_polka_any(s, r) } // 34
+            (RoundStep::Prevote, Event::PolkaNil(r)) => {  handle_polka_nil(s, r) } // 44
+            (RoundStep::Prevote, Event::PolkaValue(r, v)) => {  handle_polka_value(s, r, v) } // 36
+            (RoundStep::Prevote, Event::TimeoutPrevote(r)) => {  handle_timeout_prevote(s, r) } // 61
+            (RoundStep::Precommit, Event::PolkaValue(r, v)) => {  handle_polka_value(s, r, v) } // 36/42
+            (_,                    Event::CommitAny(r)) => {  handle_commit_any(s, r) } // 47
+            (_,                    Event::CommitValue(r, v)) => {  handle_commit_value(s, r, v) } // 49
+            (_,                    Event::CertValue(r, v)) => {  handle_cert_value(s, r, v) } // 55
+            (_,                    Event::TimeoutPrecommit(r)) => {  handle_timeout_precommit(s, r) } // 65
             _ => { (s, None) }
         };
         (self.with_state(s), m)
@@ -188,26 +181,26 @@ impl<V> StateWrapper<V>
 
     // we're the proposer. decide a propsal.
     // 11/14
-    fn handle_new_round_proposer(&self, h: i64, r: i64) -> (State, Option<Message>) {
+    fn handle_new_round_proposer(&self, r: i64) -> (State, Option<Message>) {
         let s = self.state.update_round(r).update_step(RoundStep::Propose);
         let (proposal_value, valid_round) = match s.valid {
             Some(v) => { (v.value, v.round) }
             None    => { (self.value_manager.get_value(), -1) }
         };
-        (s, Some(Message::Proposal(Proposal::new(h, r, proposal_value, valid_round))))
+        (s, Some(Message::Proposal(Proposal::new(r, proposal_value, valid_round))))
     }
 
 
     // we're not the proposer. schedule timeout propose
     // 11/20
-    fn handle_new_round(&self, h: i64, r: i64) -> (State, Option<Message>) {
+    fn handle_new_round(&self, r: i64) -> (State, Option<Message>) {
         let s = self.state.update_round(r).update_step(RoundStep::Propose);
-        (s, Some(Message::Timeout(Timeout::new(s.height, s.round, s.step))))
+        (s, Some(Message::Timeout(Timeout::new(s.round, s.step))))
     }
 
     // received a complete proposal with new value - prevote
     // 22
-    fn handle_proposal(&self, h: i64, r: i64, proposed_value: Value) -> (State, Option<Message>){
+    fn handle_proposal(&self, r: i64, proposed_value: Value) -> (State, Option<Message>){
         let s = self.state.update_step(RoundStep::Prevote);
         let prevote_value = match self.value_manager.validate(proposed_value) {
             false => { None } // its not valid, prevote nil
@@ -216,12 +209,12 @@ impl<V> StateWrapper<V>
                 _ => { Some(proposed_value) } // otherwise, prevote the value
             }
         };
-        (s, Some(Message::Prevote(Vote::new(s.height, s.round, prevote_value))))
+        (s, Some(Message::Prevote(Vote::new(s.round, prevote_value))))
     }
 
     // received a complete proposal with old (polka) value - prevote
     // 28
-    fn handle_proposal_polka(&self, h: i64, r: i64, vr: i64, proposed_value: Value) -> (State, Option<Message>) {
+    fn handle_proposal_polka(&self, r: i64, vr: i64, proposed_value: Value) -> (State, Option<Message>) {
         let s = self.state.update_step(RoundStep::Prevote);
         let prevote_value = match self.value_manager.validate(proposed_value) {
             false => { None } // its not valid, prevote nil
@@ -231,58 +224,58 @@ impl<V> StateWrapper<V>
                 _ => { None } // otherwise, prevote nil
             }
         };
-        (s, Some(Message::Prevote(Vote::new(s.height, s.round, prevote_value))))
+        (s, Some(Message::Prevote(Vote::new(s.round, prevote_value))))
     }
 }
 
 // timed out of propose - prevote nil
 // 57
-fn handle_timeout_propose(s: State, h: i64, r: i64) -> (State, Option<Message>) {
-    if s.height == h && s.round == r {
+fn handle_timeout_propose(s: State, r: i64) -> (State, Option<Message>) {
+    if s.round == r {
         let s = s.update_step(RoundStep::Prevote);
-        return (s, Some(Message::Prevote(Vote::new(h, r, None))))
+        return (s, Some(Message::Prevote(Vote::new(r, None))))
     }
     (s, None)
 }
 
 // 34
 // TODO: this should only execute once per round
-fn handle_polka_any(s: State, h: i64, r: i64) -> (State, Option<Message>) {
-    (s, Some(Message::Timeout(Timeout::new(h, r, RoundStep::Prevote))))
+fn handle_polka_any(s: State, r: i64) -> (State, Option<Message>) {
+    (s, Some(Message::Timeout(Timeout::new(r, RoundStep::Prevote))))
 }
 
 // 44
-fn handle_polka_nil(s: State, h: i64, r: i64) -> (State, Option<Message>) {
+fn handle_polka_nil(s: State, r: i64) -> (State, Option<Message>) {
     (s, None)
 }
 
 // 36
-fn handle_polka_value(s: State, h: i64, r: i64, v: Value) -> (State, Option<Message>) {
+fn handle_polka_value(s: State, r: i64, v: Value) -> (State, Option<Message>) {
     (s, None)
 }
 
 // 61
-fn handle_timeout_prevote(s: State, h: i64, r: i64) -> (State, Option<Message>) {
+fn handle_timeout_prevote(s: State, r: i64) -> (State, Option<Message>) {
     (s, None)
 }
 
 // 47
-fn handle_commit_any(s: State, h: i64, r: i64) -> (State, Option<Message>) {
+fn handle_commit_any(s: State, r: i64) -> (State, Option<Message>) {
     (s, None)
 }
 
 // 49
-fn handle_commit_value(s: State, h: i64, r: i64, v: Value) -> (State, Option<Message>) {
+fn handle_commit_value(s: State, r: i64, v: Value) -> (State, Option<Message>) {
     (s, None)
 }
 
 // 55
-fn handle_cert_value(s: State, h: i64, r: i64, v: Value) -> (State, Option<Message>) {
+fn handle_cert_value(s: State, r: i64, v: Value) -> (State, Option<Message>) {
     (s, None)
 }
 
 // 65
-fn handle_timeout_precommit(s: State, h: i64, r: i64) -> (State, Option<Message>) {
+fn handle_timeout_precommit(s: State, r: i64) -> (State, Option<Message>) {
     (s, None)
 }
 
