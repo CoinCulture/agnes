9ebea3ae5aee8a2b257e2199ded04a54f869928b
Ethan Buchman <ethan@coinculture.info>
2018-12-23 12:44:01 -0500

    set_step -> next_step; only skip_round calls set_round; various cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//---------------------------------------------------------------------
+// State
+
 // Value is what we want to agree on.
 #[derive(Copy, Clone, PartialEq, Debug)]
 pub struct Value {}
@@ -9,10 +12,14 @@ pub struct RoundValue {
     value: Value,
 }
 
-impl RoundValue {
-    pub fn new(round: i64, value: Value) -> RoundValue {
-        RoundValue { round, value }
-    }
+// Step is the step of the consensus in the round.
+#[derive(Copy, Clone, Debug, PartialEq)]
+pub enum Step {
+    NewRound,
+    Propose,
+    Prevote,
+    Precommit,
+    Commit,
 }
 
 // State is the state of the consensus state machine.
@@ -26,41 +33,66 @@ pub struct State {
 }
 
 impl State {
+    // new creates a new State at the given height.
+    pub fn new(height: i64) -> State {
+        State {
+            height: height,
+            round: 0,
+            step: Step::NewRound,
+            locked: None,
+            valid: None,
+        }
+    }
+
+    // set_round sets the State to step NewRound at the given round.
     fn set_round(self, round: i64) -> State {
         State {
             round: round,
+            step: Step::NewRound,
             ..self
         }
     }
 
-    fn set_step(self, step: Step) -> State {
+    // next_step progresses the State to the next step,
+    // stopping at precommit. To progress to Commit,
+    // call commit_step(); to reset to NewRound, call
+    // set_round().
+    fn next_step(self) -> State {
+        let step = match self.step {
+            Step::NewRound => Step::Propose,
+            Step::Propose => Step::Prevote,
+            Step::Prevote => Step::Precommit,
+            _ => self.step,
+        };
         State { step: step, ..self }
     }
 
-    fn set_locked(self, locked: Value) -> State {
+    // commit_step sets State to the Commit step.
+    // No more state transitions can take place.
+    fn commit_step(self) -> State {
         State {
-            locked: Some(RoundValue::new(self.round, locked)),
+            step: Step::Commit,
             ..self
         }
     }
 
-    fn set_valid(self, valid: Value) -> State {
-        State {
-            valid: Some(RoundValue::new(self.round, valid)),
-            ..self
-        }
+    // set_locked sets the locked value and round.
+    fn set_locked(self, value: Value) -> State {
+        let round = self.round;
+        let locked = Some(RoundValue { round, value });
+        State { locked, ..self }
+    }
+
+    // set_valid sets the valid value and round.
+    fn set_valid(self, value: Value) -> State {
+        let round = self.round;
+        let valid = Some(RoundValue { round, value });
+        State { valid, ..self }
     }
 }
 
-// Step is the step of the consensus in the round.
-#[derive(Copy, Clone, Debug, PartialEq)]
-pub enum Step {
-    NewRound,
-    Propose,
-    Prevote,
-    Precommit,
-    Commit,
-}
+//---------------------------------------------------------------------
+// Inputs (Events)
 
 // RoundEvent contains an event and its round.
 // When applied successfully, it causes a state transition
@@ -70,7 +102,7 @@ pub struct RoundEvent {
     event: Event,
 }
 
-// Event is a type of event, with any additional data.
+// Event is a type of event. It carries any relevant data.
 pub enum Event {
     NewRound,                  // Start a new round, not as proposer.
     NewRoundProposer(Value),   // Start a new round and propose the Value.
@@ -88,16 +120,41 @@ pub enum Event {
     TimeoutPrecommit,          // Timeout waiting for precommits.
 }
 
+//---------------------------------------------------------------------
+// Outputs (Messages)
+
 // Message is the output of the state machine - proposals/votes
 // to send to peers, timeouts to schedule, and an ultimate decision value.
 #[derive(Debug, PartialEq)]
 pub enum Message {
-    NewRound(i64),
-    Proposal(Proposal),
-    Prevote(Vote),
-    Precommit(Vote),
-    Timeout(Timeout),
-    Decision(RoundValue),
+    NewRound(i64),        // Move to the new round.
+    Proposal(Proposal),   // Broadcast the proposal.
+    Prevote(Vote),        // Broadcast the prevote.
+    Precommit(Vote),      // Broadcast the precommit.
+    Timeout(Timeout),     // Schedule the timeout.
+    Decision(RoundValue), // Decide the value.
+}
+
+impl Message {
+    fn proposal(round: i64, value: Value, pol_round: i64) -> Message {
+        Message::Proposal(Proposal {
+            round,
+            value,
+            pol_round,
+        })
+    }
+    fn prevote(round: i64, value: Option<Value>) -> Message {
+        Message::Prevote(Vote { round, value })
+    }
+    fn precommit(round: i64, value: Option<Value>) -> Message {
+        Message::Precommit(Vote { round, value })
+    }
+    fn timeout(round: i64, step: Step) -> Message {
+        Message::Timeout(Timeout { round, step })
+    }
+    fn decision(round: i64, value: Value) -> Message {
+        Message::Decision(RoundValue { round, value })
+    }
 }
 
 // Proposal proposes a value in a round.
@@ -109,30 +166,11 @@ pub struct Proposal {
     pol_round: i64,
 }
 
-impl Proposal {
-    fn new(round: i64, value: Value, pol_round: i64) -> Proposal {
-        Proposal {
-            round: round,
-            value: value,
-            pol_round: pol_round,
-        }
-    }
-}
-
 // Vote is a vote for a value in a round.
 #[derive(Debug, PartialEq)]
 pub struct Vote {
     round: i64,
     value: Option<Value>,
-}
-
-impl Vote {
-    fn new(round: i64, value: Option<Value>) -> Vote {
-        Vote {
-            round: round,
-            value: value,
-        }
-    }
 }
 
 // Timeout is used to schedule timeouts at different steps in the round.
@@ -142,37 +180,20 @@ pub struct Timeout {
     step: Step,
 }
 
-impl Timeout {
-    fn new(round: i64, step: Step) -> Timeout {
-        Timeout {
-            round: round,
-            step: step,
-        }
-    }
-}
+//---------------------------------------------------------------------
+// State Transition Function
 
 impl State {
-    // new creates a new State at the given height.
-    pub fn new(height: i64) -> State {
-        State {
-            height: height,
-            round: 0,
-            step: Step::NewRound,
-            locked: None,
-            valid: None,
-        }
-    }
-
-    // next progresses the state machine. It returns an updated State
+    // next transitions the state machine. It returns an updated State
     // and an optional message. Commented numbers refer to line numbers
     // in the spec paper.
     pub fn next(self, event: RoundEvent) -> (State, Option<Message>) {
         let (s, round, eround) = (self, self.round, event.round);
         let eqr = round == eround;
         match (s.step, event.event) {
-            // From NewRound. No round guards.
-            (Step::NewRound, Event::NewRoundProposer(v)) => propose(s, eround, v), // 11/14
-            (Step::NewRound, Event::NewRound) => schedule_timeout_propose(s, eround), // 11/20
+            // From NewRound. Must equal current round.
+            (Step::NewRound, Event::NewRoundProposer(v)) if eqr => propose(s, v), // 11/14
+            (Step::NewRound, Event::NewRound) if eqr => schedule_timeout_propose(s), // 11/20
 
             // From Propose. Must equal current round.
             (Step::Propose, Event::Proposal(v)) if eqr => prevote(s, v), // 22
@@ -189,7 +210,10 @@ impl State {
             // From Precommit. Must equal current round.
             (Step::Precommit, Event::PolkaValue(v)) if eqr => set_valid_value(s, v), // 36/42 - NOTE: only once?
 
-            // From all. Various round guards.
+            // From Commit. No more state transitions.
+            (Step::Commit, _) => (s, None),
+
+            // From all (except Commit). Various round guards.
             (_, Event::PrecommitAny) if eqr => schedule_timeout_precommit(s), // 47
             (_, Event::TimeoutPrecommit) if eqr => round_skip(s, eround + 1), // 65
             (_, Event::RoundSkip) if round < eround => round_skip(s, eround), // 55
@@ -200,122 +224,126 @@ impl State {
 }
 
 //---------------------------------------------------------------------
-// propose
+// Propose
 
-// we're the proposer. decide a propsal.
+// We're the proposer - propose the valid value if it exists,
+// otherwise propose the given value.
 // 11/14
-fn propose(s: State, r: i64, v: Value) -> (State, Option<Message>) {
-    let s = s.set_round(r).set_step(Step::Propose);
-    let (value, round) = match s.valid {
+fn propose(s: State, v: Value) -> (State, Option<Message>) {
+    let s = s.next_step();
+    let (value, pol_round) = match s.valid {
         Some(v) => (v.value, v.round),
         None => (v, -1),
     };
-    (s, Some(Message::Proposal(Proposal::new(r, value, round))))
+    (s, Some(Message::proposal(s.round, value, pol_round)))
 }
 
 //---------------------------------------------------------------------
-// prevote
+// Prevote
 
-// received a complete proposal with new value - prevote
+// Received a complete proposal with new value - prevote the value,
+// unless we're locked on something else.
 // 22
 fn prevote(s: State, proposed: Value) -> (State, Option<Message>) {
-    let s = s.set_step(Step::Prevote);
+    let s = s.next_step();
     let value = match s.locked {
         Some(locked) if proposed != locked.value => None, // locked on something else
         _ => Some(proposed),
     };
-    (s, Some(Message::Prevote(Vote::new(s.round, value))))
+    (s, Some(Message::prevote(s.round, value)))
 }
 
-// received a complete proposal with old (polka) value - prevote
+// Received a complete proposal with old (polka) value - prevote the value,
+// unless we're locked at a higher round.
 // 28
 fn prevote_polka(s: State, vr: i64, proposed: Value) -> (State, Option<Message>) {
-    let s = s.set_step(Step::Prevote);
+    let s = s.next_step();
     let value = match s.locked {
         Some(locked) if locked.round <= vr => Some(proposed), // unlock and prevote
         Some(locked) if locked.value == proposed => Some(proposed), // already locked on value
         _ => None,                                            // otherwise, prevote nil
     };
-    (s, Some(Message::Prevote(Vote::new(s.round, value))))
+    (s, Some(Message::prevote(s.round, value)))
 }
 
-// received a complete proposal for an empty or invalid value, or timed out.
+// Received a complete proposal for an empty or invalid value, or timed out - prevote nil.
 // 22, 57
 fn prevote_nil(s: State) -> (State, Option<Message>) {
-    let s = s.set_step(Step::Prevote);
-    (s, Some(Message::Prevote(Vote::new(s.round, None))))
+    let s = s.next_step();
+    (s, Some(Message::prevote(s.round, None)))
 }
 
 //---------------------------------------------------------------------
-// precommit
+// Precommit
 
-// 44, 61
-fn precommit_nil(s: State) -> (State, Option<Message>) {
-    let s = s.set_step(Step::Precommit);
-    (s, Some(Message::Precommit(Vote::new(s.round, None))))
-}
-
+// Received a polka for a value - precommit the value.
 // 36
 // NOTE: only one of this and set_valid_value should be called once in a round
 fn precommit(s: State, v: Value) -> (State, Option<Message>) {
-    let s = s.set_locked(v).set_valid(v).set_step(Step::Precommit);
-    (s, Some(Message::Precommit(Vote::new(s.round, Some(v)))))
+    let s = s.set_locked(v).set_valid(v).next_step();
+    (s, Some(Message::precommit(s.round, Some(v))))
+}
+
+// Received a polka for nil or timed out of prevote - precommit nil.
+// 44, 61
+fn precommit_nil(s: State) -> (State, Option<Message>) {
+    let s = s.next_step();
+    (s, Some(Message::precommit(s.round, None)))
 }
 
 //---------------------------------------------------------------------
-// schedule timeouts
+// Schedule timeouts
 
-// we're not the proposer. schedule timeout propose
+// We're not the proposer - schedule timeout propose.
 // 11/20
-fn schedule_timeout_propose(s: State, r: i64) -> (State, Option<Message>) {
-    let s = s.set_round(r).set_step(Step::Propose);
-    (s, Some(Message::Timeout(Timeout::new(s.round, s.step))))
+fn schedule_timeout_propose(s: State) -> (State, Option<Message>) {
+    let s = s.next_step();
+    (s, Some(Message::timeout(s.round, Step::Propose)))
 }
 
+// We received a polka for any - schedule timeout prevote.
 // 34
 // NOTE: this should only be called once in a round, per the spec,
 // but it's harmless to schedule more timeouts
 fn schedule_timeout_prevote(s: State) -> (State, Option<Message>) {
-    (
-        s,
-        Some(Message::Timeout(Timeout::new(s.round, Step::Prevote))),
-    )
+    (s, Some(Message::timeout(s.round, Step::Prevote)))
 }
 
+// We received +2/3 precommits for any - schedule timeout precommit.
 // 47
 fn schedule_timeout_precommit(s: State) -> (State, Option<Message>) {
-    (
-        s,
-        Some(Message::Timeout(Timeout::new(s.round, Step::Precommit))),
-    )
+    (s, Some(Message::timeout(s.round, Step::Precommit)))
 }
 
 //---------------------------------------------------------------------
-// set the valid block
+// Set the valid value.
 
+// We received a polka for a value after we already precommited.
+// Set the valid value and current round.
 // 36/42
 // NOTE: only one of this and precommit should be called once in a round
 fn set_valid_value(s: State, v: Value) -> (State, Option<Message>) {
-    let s = s.set_valid(v);
-    (s, None)
+    (s.set_valid(v), None)
 }
 
 //---------------------------------------------------------------------
-// new round or height
+// New round or height
 
+// We finished a round (timeout precommit) or received +1/3 votes
+// from a higher round. Move to the higher round.
 // 65
 fn round_skip(s: State, r: i64) -> (State, Option<Message>) {
-    (s, Some(Message::NewRound(r)))
+    (s.set_round(r), Some(Message::NewRound(r)))
 }
 
+// We received +2/3 precommits for a value - commit and decide that value!
 // 49
 fn commit(s: State, r: i64, v: Value) -> (State, Option<Message>) {
-    let s = s.set_step(Step::Commit);
-    (s, Some(Message::Decision(RoundValue::new(r, v))))
+    (s.commit_step(), Some(Message::decision(r, v)))
 }
 
 //---------------------------------------------------------------------
-// test
+// Test
 
 #[cfg(test)]
 mod tests {
@@ -326,26 +354,26 @@ mod tests {
         let val = Value {};
         let v = Some(val);
         let s = State::new(1);
-        let (s, m) = s.next(Event {
+        let (s, m) = s.next(RoundEvent {
             round: 0,
             event: Event::NewRoundProposer(val),
         });
-        assert_eq!(m.unwrap(), Message::Proposal(Proposal::new(0, val, -1)));
-        let (s, m) = s.next(Event {
+        assert_eq!(m.unwrap(), Message::proposal(0, val, -1));
+        let (s, m) = s.next(RoundEvent {
             round: 0,
             event: Event::Proposal(val),
         });
-        assert_eq!(m.unwrap(), Message::Prevote(Vote::new(0, v)));
-        let (s, m) = s.next(Event {
+        assert_eq!(m.unwrap(), Message::prevote(0, v));
+        let (s, m) = s.next(RoundEvent {
             round: 0,
             event: Event::PolkaValue(val),
         });
-        assert_eq!(m.unwrap(), Message::Precommit(Vote::new(0, v)));
-        let (s, m) = s.next(Event {
+        assert_eq!(m.unwrap(), Message::precommit(0, v));
+        let (s, m) = s.next(RoundEvent {
             round: 0,
             event: Event::PrecommitValue(val),
         });
-        assert_eq!(m.unwrap(), Message::Decision(RoundValue::new(0, val)));
+        assert_eq!(m.unwrap(), Message::decision(0, val));
 
         assert_eq!(s.step, Step::Commit);
     }
